0601cd6373c7ee8dc552bc03c0941d7ac9ef0e16
marcelomrad <marcelomrad@gmail.com>
2024-02-23 10:56:25 -0300

    feat: introduction

---
+++ projeto-antigo/src/app/cursos/cursos.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { CursosService } from './cursos.service';
+
+@Component({
+  selector: 'app-cursos',
+  templateUrl: './cursos.component.html',
+  styleUrl: './cursos.component.css'
+})
+export class CursosComponent {
+  nomePortal: string;
+
+
+  cursos: string[] = [];
+
+  constructor(private cursosService: CursosService) {
+    this.nomePortal = 'http://celim.training';
+
+    this.cursos = this.cursosService.getCursos();
+
+  }
+
+}
